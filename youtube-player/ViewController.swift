1a383606603de92e685e4732629c703b06bf7ed9
GregoireLgt <old.times.aka.liebe@gmail.com>
2020-07-30 23:20:11 +0200

    first xcode project creation

---
+++ youtube-player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-player
+//
+//  Created by Grégoire Laget on 30/07/2020.
+//  Copyright © 2020 Grégoire Laget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
