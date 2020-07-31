2aaed97096bcd2f72e49d14375b4b64006c53f83
GregoireLgt <old.times.aka.liebe@gmail.com>
2020-07-31 16:32:19 +0200

    added table view, custom protocol, displaying video titles

--- youtube-player/Model.swift
+++ youtube-player/Model.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    func videosFetched(_ video:[Video])
+}
+
 class Model {
+    
+    var delegate: ModelDelegate?
     
     func getVideos(){
         
@@ -42,6 +48,18 @@ class Model {
                 decoder.dateDecodingStrategy = .iso8601 //if dates are in the JSON, the format is iso8601
                 
                 let response = try decoder.decode(Response.self, from: data!)
+                
+                
+                
+                if response.items != nil {
+                    
+                    
+                    DispatchQueue.main.async {
+                        // Call the "videosFetched" method of the delegate
+                        
+                        self.delegate?.videosFetched(response.items!)
+                    }
+                }
                 
                 dump(response)
             }
